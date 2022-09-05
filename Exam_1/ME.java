c66e8bcecf000897fc344871123efb1a38c889f1
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2022-09-05 09:51:36 +0800

    Create ME.java

---
+++ Exam_1/ME.java
@@ -0,0 +1,21 @@
+public class ME {
+    String surname;
+    String firstname;
+    String dateOfBirth;
+    char middleInitial;
+    int studentNumber;
+    String studentEmailAddress;
+    boolean IamAwsome;  
+    
+    public void sayMystudentnumber() {
+        System.out.println("Student Numeber:"+ studentNumber);
+    }
+    public void sayMystudentEmailAddress() {
+        System.out.println("Student Email Address:"+studentEmailAddress);
+    }
+    public void IamAwsome() {
+        System.out.println("I am Awsome= "+IamAwsome +"\n");
+    }
+    
+    
+}
