1368c51ee0b2abf6ced21d05ec29c24ae185977a
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2022-09-05 09:55:00 +0800

    Update app.java

--- Exam_1/app.java
+++ Exam_1/app.java
@@ -1,21 +1,15 @@
-public class ME {
-    String surname;
-    String firstname;
-    String dateOfBirth;
-    char middleInitial;
-    int studentNumber;
-    String studentEmailAddress;
-    boolean IamAwsome;  
-    
-    public void sayMystudentnumber() {
-        System.out.println("Student Numeber:"+ studentNumber);
+public class App {
+    public static void main(String[] args) throws Exception {
+    ME ME = new ME();
+    ME . surname = "Umengan";
+    ME . firstname = "Darwin";
+    ME . middleInitial = 'F';
+    ME . dateOfBirth = "02/17/2004";
+    ME . studentNumber = 2022101822;
+    ME . studentEmailAddress = "umengandf@students.national-u.edu.ph";
+    ME . IamAwsome = true;
+    ME . sayMystudentnumber();
+    ME . sayMystudentEmailAddress();
+    ME. IamAwsome();
     }
-    public void sayMystudentEmailAddress() {
-        System.out.println("Student Email Address:"+studentEmailAddress);
-    }
-    public void IamAwsome() {
-        System.out.println("I am Awsome= "+IamAwsome +"\n");
-    }
-    
-    
 }
